f6a06ea76d624b34f344d99c419f4235fdae64d2
CSNR Ganesh <100460284+meihu-ganesh@users.noreply.github.com>
2023-03-25 14:11:37 +0530

    issue raised

--- src/components/AddNewItems.js
+++ src/components/AddNewItems.js
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 
 const AddNewItems = () => {
     const [inputVal, setInputVal] = useState("");
+    const [isFormVisible, setIsFormVisible] = useState(false);
     const dispatch = useDispatch();
     const submitHandler = (e) => {
     e.preventDefault();
@@ -14,13 +15,22 @@ const AddNewItems = () => {
     const updateInput = (e) => {
     setInputVal(e.target.value);
     };
+    const openForm = ()=> {
+        setIsFormVisible(true);
+    }
+    const hideForm = ()=> {
+        setIsFormVisible(false);
+    }
     return (
     <div>
-        <button>+ Add New Card</button>
-        <form onSubmit={submitHandler}>
-        <input value={inputVal} onChange={updateInput} />
-        <button onClick={submitHandler}>Save</button>
-        </form>
+        <button onClick={openForm}>+ Add New Card</button>
+        { isFormVisible && <form onSubmit={submitHandler} className='mt-3'>
+            <div className=" mt-3">
+                <input value={inputVal} onChange={updateInput} />
+                <button onClick={hideForm} className="mr-3">Cancel</button>
+                <button onClick={submitHandler} className='bg-blue-500 px-3 py-1'>Save</button>
+            </div>
+        </form>}
     </div>
     );
 };
--- src/components/List.js
+++ src/components/List.js
@@ -7,7 +7,7 @@ const List = () => {
     const listItem = useSelector(store => store.listSlice.list);
     return (
         <>
-            {listItem.length && listItem.map((list)=><div key = { list.id } className='p-3 bg-gray-200 w-1/3'>{ list.title }</div>)}
+            { listItem.map((list)=><div key = { list.id } className='p-3 bg-gray-200 w-1/3'>{ list.title }</div>) }
             <div className='p-3 bg-gray-200 w-1/3'>
                 {/* <div>To Do</div> */}
                 {/* <Card /> */}
